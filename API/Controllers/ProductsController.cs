1f9416053fef36d8f31027cb431d9dc507049acd
btong924 <btong924@gmail.com>
2020-09-30 16:35:40 +0700

    hendler error exception middleware

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> productRepo;
         private readonly IGenericRepository<ProductBrand> productBrandRepo;
