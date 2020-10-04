cd8193a54f83879bb1be4400f54ce62b2ad1565e
btong924 <btong924@gmail.com>
2020-10-04 11:04:25 +0700

    End of section 13

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await basketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updateBasket = await basketRepository.UpdateBasketAsync(basket);
+            return Ok(updateBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id)
+        {
+            await basketRepository.DeleteBasketAsync(id);
+        }
+
+
+    }
+}
