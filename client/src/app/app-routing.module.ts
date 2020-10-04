b39c8f5155e4ad272a8955e8a55c24aa8e57c87b
btong924 <btong924@gmail.com>
2020-10-04 23:14:40 +0700

    End of section 14

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' }, },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
   {
     path: 'test-error',
     component: TestErrorComponent,
@@ -27,6 +27,18 @@ const routes: Routes = [
     loadChildren: () =>
       import('./shop/shop.module').then((mod) => mod.ShopModule),
     data: { breadcrumb: 'Shop' },
+  },
+  {
+    path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+    data: { breadcrumb: 'Basket' },
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((mod) => mod.CheckoutModule),
+    data: { breadcrumb: 'Checkout' },
   },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
